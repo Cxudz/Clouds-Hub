f0ad74155c088def4492104182546aef3b37c0bb
Cxudz <112773914+Cxudz@users.noreply.github.com>
2022-09-04 17:20:01 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -60,7 +60,7 @@ local themeStyles = {
         SchemeColor = Color3.fromRGB(64, 64, 64),
         Background = Color3.fromRGB(0, 0, 0),
         Header = Color3.fromRGB(0, 0, 0),
-        TextColor = Color3.fromRGB(255, 255, 255),
+        TextColor = Color3.fromRGB(173, 3, 252),
         ElementColor = Color3.fromRGB(20, 20, 20)
     },
     LightTheme = {
